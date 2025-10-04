27d1b20c73b028169d0ffe6c91c36a93bacda452
Pranit <pranitrustagi@gmail.com>
2025-10-04 18:48:15 +0530

    landing page set to signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import { Dashboard } from './pages/Dashboard';
 import { SendMoney } from './pages/SendMoney';
 import { Signin } from './pages/Signin';
 import { Signup } from './pages/Signup';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <>
        <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/signup" />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
